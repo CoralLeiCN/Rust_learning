61fa5f7b4c044485daee3bc18a78eb28ed0eda5e
CoralLeiCN <iverson930g@gmail.com>
2024-05-27 23:42:47 +0100

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is: {x}");
-    x = 6;
+    let x = 6;
     println!("The value of x is: {x}");
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    println!("The value of x is: {x}");
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
+    // error message
+    // ^^^^^^^^^^^^ expected `&str`, found `usize`
+
+    let spaces = "   ";
+    let spaces = spaces.len();
 }
