0d38ac4e9a4bc01f5030ee97c2efcd3792ea7c77
CoralLeiCN <iverson930g@gmail.com>
2024-05-30 23:53:01 +0100

    add expression

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,24 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    print_number(5);
+    print_labeled_measurement(5, 'h');
+
+    let y = 6; //statements
+    let y = {
+        let x = 3;
+        x + 1
+    }; //expressions
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn print_number(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
