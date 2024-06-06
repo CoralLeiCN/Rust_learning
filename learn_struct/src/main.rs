a4117c7fbe675f6bd9ff873b9a3cc64c28454ad4
CoralLeiCN <iverson930g@gmail.com>
2024-06-07 00:19:25 +0100

    creating instances from other instances

--- learn_struct/src/main.rs
+++ learn_struct/src/main.rs
@@ -15,13 +15,30 @@ fn main() {
     user1.email = String::from("anotheremail@example.com");
 
     let user2 = build_user("asd".to_string(), "asd".to_string());
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("someusername123"),
+        ..user1 // must come last to specify that any remaining fields
+    };
+
+    let user3 = User {
+        email: String::from("another@example.com"),
+        ..user1 // Both active and sign_in_count are types that implement the Copy trait
+    };
 }
 
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
-        username: username,
-        email: email,
+        username,
+        email,
         sign_in_count: 1,
     }
 }
