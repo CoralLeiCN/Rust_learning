107f69c387b271c0dbb51e1f3cc8fac33ea4d031
CoralLeiCN <iverson930g@gmail.com>
2024-06-07 08:21:00 +0100

    tuple and unit struct

--- learn_struct/src/main.rs
+++ learn_struct/src/main.rs
@@ -32,6 +32,17 @@ fn main() {
         email: String::from("another@example.com"),
         ..user1 // Both active and sign_in_count are types that implement the Copy trait
     };
+
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    {
+        let black = Color(0, 0, 0);
+        let origin = Point(0, 0, 0);
+    }
+
+    struct AlwaysEqual;
+    let subject = AlwaysEqual;
 }
 
 fn build_user(email: String, username: String) -> User {
