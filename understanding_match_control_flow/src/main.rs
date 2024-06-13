5f2333caea6278771ca05d7566ce5c250284bcb1
CoralLeiCN <iverson930g@gmail.com>
2024-06-13 23:59:04 +0100

    match with option<T>

--- understanding_match_control_flow/src/main.rs
+++ understanding_match_control_flow/src/main.rs
@@ -27,8 +27,27 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
     let a_coin = Coin::Penny;
     let result = value_in_cents(a_coin);
     println!("The value of the coin is: {}", result);
+
+    let a_state = UsState::Alaska;
+    let b_coin = Coin::Quarter(a_state);
+    let result1 = value_in_cents(b_coin);
+    println!("The value of the coin is: {}", result1);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("The value of five is: {:?}", five);
+    println!("The value of six is: {:?}", six);
+    println!("The value of none is: {:?}", none);
 }
