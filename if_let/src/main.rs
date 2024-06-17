1869af1a7ed2c1e52ee3111b63591266cd49ed0e
CoralLeiCN <iverson930g@gmail.com>
2024-06-17 22:17:19 +0100

    else with an if let

--- if_let/src/main.rs
+++ if_let/src/main.rs
@@ -11,8 +11,30 @@ fn if_let_max(config_max: &Option<u8>) {
     }
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     let config_max = Some(3u8);
     match_max(&config_max);
     if_let_max(&config_max);
+
+    let mut count: i32 = 0;
+    let coin = Coin::Quarter(UsState::Alaska);
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {state:?}!");
+    } else {
+        count += 1;
+    }
 }
