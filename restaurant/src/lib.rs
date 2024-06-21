fc1d279617245dcf27ed9e7691a2dfbab9fd869f
CoralLeiCN <iverson930g@gmail.com>
2024-06-21 23:58:12 +0100

    cannot import private

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn eat_at_restaurant() {
+    // error[E0603]: module `hosting` is private
+    // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // error[E0603]: module `hosting` is private
+    // Relative path
+    // front_of_house::hosting::add_to_waitlist();
 }
