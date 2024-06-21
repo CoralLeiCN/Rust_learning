44902be446c29f5a5133368a05098f0984244e8a
CoralLeiCN <iverson930g@gmail.com>
2024-06-22 00:19:26 +0100

    make module and function public

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,16 +1,23 @@
+// error[E0603]: module `hosting` is private
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
+
+//         fn seat_at_table() {}
+//     }
+
+//     mod serving {
+//         fn take_order() {}
+
+//         fn serve_order() {}
+
+//         fn take_payment() {}
+//     }
+// }
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
 }
 
@@ -22,4 +29,6 @@ pub fn eat_at_restaurant() {
     // error[E0603]: module `hosting` is private
     // Relative path
     // front_of_house::hosting::add_to_waitlist();
+    crate::front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
 }
