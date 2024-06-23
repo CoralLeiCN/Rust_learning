e061b85206134c98e1b80ca6e4722220dace322e
CoralLeiCN <iverson930g@gmail.com>
2024-06-23 16:04:56 +0100

    Enum to Store Multiple Types

--- C81_vector/src/main.rs
+++ C81_vector/src/main.rs
@@ -1,3 +1,9 @@
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
     // specify the type within angle brackets.
     let v: Vec<i32> = Vec::new();
@@ -33,13 +39,30 @@ fn main() {
 
     let v = vec![1, 2, 3, 4, 5];
     // [] method will cause the program to panic
-    let does_not_exist = &v[100];
+    // let does_not_exist = &v[100];
     // When the get method is passed an index that is outside the vector, it returns None without panicking.
-    let does_not_exist = v.get(100);
+    // let does_not_exist = v.get(100);
 
     // // error[E0502]: cannot borrow `v` as mutable because it is also borrowed as immutable
     // let mut v5 = vec![1, 2, 3, 4, 5];
     // let first = &v5[0]; //-- immutable borrow occurs here
     // v5.push(6); //mutable borrow occurs here
     // println!("The first element is: {first}");
+
+    let v5 = vec![100, 32, 57];
+    for i in &v5 {
+        println!("{i}");
+    }
+
+    let mut v6 = vec![100, 32, 57];
+    for i in &mut v6 {
+        *i += 50;
+        println!("{i}");
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
