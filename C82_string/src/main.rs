ca5c72b60d73b33fc079532f8f79a74a07667ff8
CoralLeiCN <iverson930g@gmail.com>
2024-06-29 21:09:27 +0100

    Updating a String

--- C82_string/src/main.rs
+++ C82_string/src/main.rs
@@ -25,4 +25,14 @@ fn main() {
     let mut s = String::from("foo");
     s.push_str("bar");
     println!("{}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {s1}");
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    //The push method takes single character
+    s.push('l');
 }
