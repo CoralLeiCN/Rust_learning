310cbc0e880ee138ab73b20159d8ee4467134caa
CoralLeiCN <iverson930g@gmail.com>
2024-06-29 21:40:32 +0100

    concatenation string

--- C82_string/src/main.rs
+++ C82_string/src/main.rs
@@ -35,4 +35,28 @@ fn main() {
     let mut s = String::from("lo");
     //The push method takes single character
     s.push('l');
+
+    let ss1 = String::from("Hello, ");
+    let ss2 = String::from("world!");
+    let ss3 = ss1 + &ss2; // note s1 has been moved here and can no longer be used
+
+    // println!("ss1 is {ss1}"); ss1 is no longer valid after addition
+    println!("ss2 is {ss2}");
+    println!("ss3 is {ss3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let sss1 = String::from("tic");
+    let sss2 = String::from("tac");
+    let sss3 = String::from("toe");
+
+    let sss = format!("{sss1}-{sss2}-{sss3}");
+    println!("sss1 is {sss1}");
+    println!("sss2 is {sss2}");
+    println!("sss3 is {sss3}");
+    println!("sss is {sss}");
 }
