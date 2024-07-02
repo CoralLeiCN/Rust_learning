f6d2634d0e6b0463ed5da503eb01c8c743882466
CoralLeiCN <iverson930g@gmail.com>
2024-07-03 00:11:15 +0100

    unwrap_or_else

--- C92_recoverable_errors_with_result/src/main.rs
+++ C92_recoverable_errors_with_result/src/main.rs
@@ -21,4 +21,14 @@ fn main() {
             }
         },
     };
+
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
 }
