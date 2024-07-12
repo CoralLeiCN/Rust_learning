7d8e2c7546b4536ee1ba1c63c02fbd9245b8573f
CoralLeiCN <iverson930g@gmail.com>
2024-07-12 23:52:28 +0100

    add equivalent code for guess

--- C93_to_panic_or_not_to_panic/src/main.rs
+++ C93_to_panic_or_not_to_panic/src/main.rs
@@ -6,6 +6,23 @@ fn main() {
         .expect("Hardcoded IP address should be valid");
 }
 
+// loop {
+//     // --snip--
+
+//     let guess: i32 = match guess.trim().parse() {
+//         Ok(num) => num,
+//         Err(_) => continue,
+//     };
+
+//     if guess < 1 || guess > 100 {
+//         println!("The secret number will be between 1 and 100.");
+//         continue;
+//     }
+
+//     match guess.cmp(&secret_number) {
+//         // --snip--
+// }
+
 pub struct Guess {
     value: i32,
 }
