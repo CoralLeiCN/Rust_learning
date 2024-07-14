bd8c2b6cbd2c1b0dd71597bd1deb7d1dc0cdd127
CoralLeiCN <iverson930g@gmail.com>
2024-07-14 23:03:41 +0100

    generic types different from its struct’s definition

--- C100_genetic_types_traits_lifetimes/src/main.rs
+++ C100_genetic_types_traits_lifetimes/src/main.rs
@@ -65,6 +65,20 @@ struct Point_multi_generics<T, U> {
     y: U,
 }
 
+struct Point1<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point1<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point1<X2, Y2>) -> Point1<X1, Y2> {
+        Point1 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -97,4 +111,11 @@ fn main() {
 
     let p = Point { x: 5.0, y: 4.0 };
     println!("distance_from_origin = {}", p.distance_from_origin());
+
+    let p1 = Point1 { x: 5, y: 10.4 };
+    let p2 = Point1 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
