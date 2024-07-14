f78013d928836197425d059a3cb8ab8e6bd7ac98
CoralLeiCN <iverson930g@gmail.com>
2024-07-14 21:56:14 +0100

    find largest differ only in their names and the types in their signatures

--- C100_genetic_types_traits_lifetimes/src/main.rs
+++ C100_genetic_types_traits_lifetimes/src/main.rs
@@ -8,6 +8,19 @@ fn find_largest_number(number_list: &[i32]) -> &i32 {
     }
     largest
 }
+
+fn find_largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -20,4 +33,9 @@ fn main() {
     let largest = find_largest_number(&number_list);
 
     println!("The largest number is {largest}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = find_largest_char(&char_list);
+    println!("The largest char is {result}");
 }
