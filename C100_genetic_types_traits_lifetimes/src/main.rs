69dd5080354cff0e8318400f0a6a675e4efba991
CoralLeiCN <iverson930g@gmail.com>
2024-07-14 21:20:16 +0100

    Abstracted code to find the largest number in two lists

--- C100_genetic_types_traits_lifetimes/src/main.rs
+++ C100_genetic_types_traits_lifetimes/src/main.rs
@@ -1,37 +1,23 @@
-fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-
-    // let mut largest = &number_list[0];
-
-    // for number in &number_list {
-    //     if number > largest {
-    //         largest = number;
-    //     }
-    // }
-
-    // println!("The largest number is {largest}");
-
-    let number_list = vec![34, 50, 25, 100, 65];
-
+fn find_largest_number(number_list: &[i32]) -> &i32 {
     let mut largest = &number_list[0];
 
-    for number in &number_list {
+    for number in number_list {
         if number > largest {
             largest = number;
         }
     }
+    largest
+}
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    println!("The largest number is {largest}");
+    let largest = find_largest_number(&number_list);
+
+    println!("The largest numberc is {largest}");
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    let largest = find_largest_number(&number_list);
 
     println!("The largest number is {largest}");
 }
