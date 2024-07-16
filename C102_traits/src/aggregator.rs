b7a19a1c1d5828202cfa4ca6d7a37fafa3458336
CoralLeiCN <iverson930g@gmail.com>
2024-07-16 22:40:38 +0100

    default implementation of a trait

--- C102_traits/src/aggregator.rs
+++ C102_traits/src/aggregator.rs
@@ -1,6 +1,9 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
+
 pub struct NewsArticle {
     pub headline: String,
     pub location: String,
