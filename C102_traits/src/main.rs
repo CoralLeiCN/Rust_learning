7cf98a53f99aeef86525585921ecdbb4d3164047
CoralLeiCN <iverson930g@gmail.com>
2024-07-16 22:33:23 +0100

    create aggregator library crate

--- C102_traits/src/main.rs
+++ C102_traits/src/main.rs
@@ -1,29 +1,14 @@
-pub trait Summary {
-    fn summarize(&self) -> String;
-}
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
+pub mod aggregator;
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
+use crate::aggregator::{Summary, Tweet};
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
+    println!("1 new tweet: {}", tweet.summarize());
 }
-fn main() {}
