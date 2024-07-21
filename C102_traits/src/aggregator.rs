29b56f9d8625e7275fd28601d1a486cff6841bf5
CoralLeiCN <iverson930g@gmail.com>
2024-07-21 22:46:09 +0100

    Conditionally implementing methods on a generic type depending on trait bounds

--- C102_traits/src/aggregator.rs
+++ C102_traits/src/aggregator.rs
@@ -59,8 +59,6 @@ impl Summary for Tweet {
     }
 }
 
-
-
 //returns some type that implements the Summary trait without naming the concrete type.
 fn returns_summarizable() -> impl Summary {
     Tweet {
@@ -93,3 +91,26 @@ fn returns_summarizable() -> impl Summary {
 //         }
 //     }
 // }
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
