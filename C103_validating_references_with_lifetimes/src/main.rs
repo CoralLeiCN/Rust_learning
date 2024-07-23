1954a216b9d6810d97d01225dedec2137b734de0
CoralLeiCN <iverson930g@gmail.com>
2024-07-23 23:14:57 +0100

    the longest function definition specifying that all the references in the signature must have the same lifetime 'a

--- C103_validating_references_with_lifetimes/src/main.rs
+++ C103_validating_references_with_lifetimes/src/main.rs
@@ -9,6 +9,22 @@
 
 //     println!("r: {r}");
 // }
+// error[E0106]: missing lifetime specifier
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
 
 fn main() {
     let x = 5; // ----------+-- 'b
@@ -17,4 +33,10 @@ fn main() {
                 //   |       |
     println!("r: {r}"); //   |       |
                         // --+       |
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
 } // ----------+
