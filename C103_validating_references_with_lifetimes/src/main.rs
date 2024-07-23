b22d2b9aabcc89796af77ed518c6d41f7a1c75e1
CoralLeiCN <iverson930g@gmail.com>
2024-07-23 23:29:01 +0100

    Attempting to use result after string2 has gone out of scope

--- C103_validating_references_with_lifetimes/src/main.rs
+++ C103_validating_references_with_lifetimes/src/main.rs
@@ -39,4 +39,20 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {result}");
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
+    }
+
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {result}");
 } // ----------+
