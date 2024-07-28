bfac114124a49a1ceff084ba8c2de504c0518f99
CoralLeiCN <iverson930g@gmail.com>
2024-07-28 20:02:47 +0100

    use assert! macro in tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,18 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -20,5 +32,19 @@ mod tests {
     #[test]
     fn another() {
         panic!("Make this test fail");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
