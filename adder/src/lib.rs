9b6ac31e9f446ab0fa2215ad78f6f2af6b74bab0
CoralLeiCN <iverson930g@gmail.com>
2024-07-30 23:29:48 +0100

    Using Result<T, E> in Tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -42,7 +42,14 @@ impl Guess {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    #[test]
+    fn it_works2() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
     #[test]
     #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
