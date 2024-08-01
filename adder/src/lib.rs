185dda7076e43ad3debd6b9ddaabc1fac711dbd6
CoralLeiCN <iverson930g@gmail.com>
2024-08-01 23:16:54 +0100

    Tests for a function that calls println!

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -44,7 +44,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works2() -> Result<(), String> {
-        if 2 + 2 == 4 {
+        if add_two(2) == 4 {
             Ok(())
         } else {
             Err(String::from("two plus two does not equal four"))
