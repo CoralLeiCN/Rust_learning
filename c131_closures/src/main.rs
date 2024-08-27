faf84d43cd95fc32a9750bc775e54976184f9778
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-08-27 21:39:40 +0100

    Calling the map method to create a new iterator and then calling the collect method to consume the new iterator and create a vector

--- c131_closures/src/main.rs
+++ c131_closures/src/main.rs
@@ -161,6 +161,9 @@ fn main() {
         //Iterator adaptors
         let v1: Vec<i32> = vec![1, 2, 3];
         v1.iter().map(|x| x + 1);
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        println!("{v2:#?}");
+        assert_eq!(v2, vec![2, 3, 4]);
     }
 }
 
