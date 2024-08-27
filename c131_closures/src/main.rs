56ebe74a3d5e64819e984a3d378d761e44381450
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-08-27 21:37:53 +0100

    Calling the iterator adaptor map to create a new iterator

--- c131_closures/src/main.rs
+++ c131_closures/src/main.rs
@@ -158,6 +158,9 @@ fn main() {
         for val in v1_iter {
             println!("Got: {val}");
         }
+        //Iterator adaptors
+        let v1: Vec<i32> = vec![1, 2, 3];
+        v1.iter().map(|x| x + 1);
     }
 }
 
