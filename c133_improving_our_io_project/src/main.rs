b7af04843178c220c28b62df412f114674dd8842
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-08-29 21:57:16 +0100

    Changing the body of Config::build to use iterator methods

--- c133_improving_our_io_project/src/main.rs
+++ c133_improving_our_io_project/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use c133_improving_our_io_project::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    // --snip--
+
+    if let Err(e) = c133_improving_our_io_project::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
