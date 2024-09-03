2f08f1e1df66ad5a5e0310c7026cbbc2806ae4e7
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-03 23:38:52 +0100

    A documentation comment for a function

---
+++ 14.2_Publishing_a_Crate_to_Crates.io/example/src/lib.rs
@@ -0,0 +1,14 @@
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// use example::*;
+/// let arg = 5;
+/// let answer = add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
