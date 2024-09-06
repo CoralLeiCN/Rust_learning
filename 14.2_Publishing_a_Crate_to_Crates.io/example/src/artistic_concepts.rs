a2c9a2cae48427d3d0a05f04c2aa0e3b3da46bcf
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-06 23:48:05 +0100

    Adding pub use statements to re-export items

--- 14.2_Publishing_a_Crate_to_Crates.io/example/src/artistic_concepts.rs
+++ 14.2_Publishing_a_Crate_to_Crates.io/example/src/artistic_concepts.rs
@@ -2,6 +2,11 @@
 //!
 //! A library for modeling artistic concepts.
 
+// pub use statements to re-export the items at the top level,
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
 pub mod kinds {
     /// The primary colors according to the RYB color model.
     pub enum PrimaryColor {
