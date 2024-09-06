d32ea848f3720ce83ff0b790f5ba00288440d708
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-06 23:50:52 +0100

    A program using the re-exported items from the artistic_concepts crate

--- 14.2_Publishing_a_Crate_to_Crates.io/example/src/main.rs
+++ 14.2_Publishing_a_Crate_to_Crates.io/example/src/main.rs
@@ -1,5 +1,9 @@
-use example::artistic_concepts::kinds::PrimaryColor;
-use example::artistic_concepts::utils::mix;
+// use example::artistic_concepts::kinds::PrimaryColor;
+// use example::artistic_concepts::utils::mix;
+
+// use re-export items
+use artistic_concepts::mix;
+use artistic_concepts::PrimaryColor;
 
 fn main() {
     let red = PrimaryColor::Red;
