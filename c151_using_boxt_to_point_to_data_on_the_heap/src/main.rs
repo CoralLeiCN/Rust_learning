86efba2124792ae83c4019e287774e9e33da7c33
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-09 19:46:44 +0100

    Size of a Non-Recursive Type

--- c151_using_boxt_to_point_to_data_on_the_heap/src/main.rs
+++ c151_using_boxt_to_point_to_data_on_the_heap/src/main.rs
@@ -2,6 +2,12 @@ enum List {
     Cons(i32, Box<List>),
     Nil,
 }
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
 use crate::List::{Cons, Nil};
 
