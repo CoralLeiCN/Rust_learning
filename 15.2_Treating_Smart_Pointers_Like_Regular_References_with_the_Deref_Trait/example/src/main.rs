21c2cea64834f25cab1635fca1918a6076b83821
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-10 23:51:03 +0100

    test reference on MyBox

--- 15.2_Treating_Smart_Pointers_Like_Regular_References_with_the_Deref_Trait/example/src/main.rs
+++ 15.2_Treating_Smart_Pointers_Like_Regular_References_with_the_Deref_Trait/example/src/main.rs
@@ -1,4 +1,11 @@
 use std::ops::Deref;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
 
 impl<T> Deref for MyBox<T> {
     type Target = T;
@@ -12,8 +19,10 @@ fn main() {
     let x = 5;
     let y = &x;
     let y_box = Box::new(x);
+    let y_mybox = MyBox::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
     assert_eq!(5, *y_box);
+    assert_eq!(5, *y_mybox);
 }
