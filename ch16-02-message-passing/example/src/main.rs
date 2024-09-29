dc3df09a8c0d2b187ba92042153124ba0d214bca
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-29 23:16:34 +0100

    Sending multiple messages and pausing between each

--- ch16-02-message-passing/example/src/main.rs
+++ ch16-02-message-passing/example/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
@@ -7,8 +8,32 @@ fn main() {
     thread::spawn(move || {
         let val = String::from("hi");
         tx.send(val).unwrap();
+        // error[E0382]: borrow of moved value: `val`
+        // println!("val is {val}");
     });
 
     let received = rx.recv().unwrap();
     println!("Got: {received}");
+
+    {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("Got: {received}");
+        }
+    }
 }
