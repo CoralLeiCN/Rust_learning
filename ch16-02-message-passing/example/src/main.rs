1565d72da473c81309baadf90dd2813ea9fe2663
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-09-30 23:51:00 +0100

    Sending multiple messages from multiple producers

--- ch16-02-message-passing/example/src/main.rs
+++ ch16-02-message-passing/example/src/main.rs
@@ -2,6 +2,47 @@ use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
+fn multiple_producers() {
+    // --snip--
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+
+    // --snip--
+}
+
 fn main() {
     let (tx, rx) = mpsc::channel();
 
@@ -35,5 +76,11 @@ fn main() {
         for received in rx {
             println!("Got: {received}");
         }
+    }
+
+    println!("multiple producers");
+
+    {
+        multiple_producers();
     }
 }
