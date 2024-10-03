4a2ce9e108cafc9c83bd54a0c1c2a938be792741
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-03 23:25:34 +0100

    An alternate implementation of the Screen struct and its run method using generics and trait bounds

--- ch17-02-trait-objects/example/src/lib.rs
+++ ch17-02-trait-objects/example/src/lib.rs
@@ -13,3 +13,19 @@ impl Screen {
         }
     }
 }
+
+// An alternate implementation of the Screen struct and its run method using generics and trait bounds
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen<T>
+where
+    T: Draw,
+{
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
