d15268a0541ba617d47efb91f13f2c339994bade
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-04 23:20:09 +0100

    A Button struct that implements the Draw trait

--- ch17-02-trait-objects/example/src/lib.rs
+++ ch17-02-trait-objects/example/src/lib.rs
@@ -14,18 +14,30 @@ impl Screen {
     }
 }
 
-// An alternate implementation of the Screen struct and its run method using generics and trait bounds
-pub struct Screen<T: Draw> {
-    pub components: Vec<T>,
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
 }
 
-impl<T> Screen<T>
-where
-    T: Draw,
-{
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
-        }
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
     }
 }
+
+// An alternate implementation of the Screen struct and its run method using generics and trait bounds
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//     T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
