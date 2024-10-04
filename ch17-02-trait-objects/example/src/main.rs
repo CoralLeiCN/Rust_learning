ee8be620750258c7d9440c9d0d336c5d82fdd105
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-04 23:55:28 +0100

    Another crate using gui and implementing the Draw trait on a SelectBox struct

--- ch17-02-trait-objects/example/src/main.rs
+++ ch17-02-trait-objects/example/src/main.rs
@@ -1,3 +1,20 @@
+mod gui;
+use gui::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
+// use crate::front_of_house::hosting::add_to_waitlist;
