4707f9c516f65c988aed2d016d970e7fe502c4a7
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-19 16:22:12 +0100

    Matching Ranges of Values with ..=

--- ch18-03-pattern-syntax/example/src/main.rs
+++ ch18-03-pattern-syntax/example/src/main.rs
@@ -17,4 +17,11 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
 }
