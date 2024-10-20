9c1c6aec18869ead635d9904acffc9e068c0b989
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-20 22:52:45 +0100

    'c' is within the first pattern’s range

--- ch18-03-pattern-syntax/example/src/main.rs
+++ ch18-03-pattern-syntax/example/src/main.rs
@@ -24,4 +24,12 @@ fn main() {
         1..=5 => println!("one through five"),
         _ => println!("something else"),
     }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
 }
