6d7432c08898d2af36d313340ee08c4173e494db
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-23 23:18:40 +0100

    Matching on nested enums

--- ch18-03-pattern-syntax/example/src/main.rs
+++ ch18-03-pattern-syntax/example/src/main.rs
@@ -10,6 +10,18 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 
+enum Message1 {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 fn main() {
     let x = Some(5);
     let y = 10;
@@ -82,5 +94,17 @@ fn main() {
         Message::ChangeColor(r, g, b) => {
             println!("Change the color to red {r}, green {g}, and blue {b}")
         }
+    }
+
+    let msg = Message1::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message1::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        Message1::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}")
+        }
+        _ => (),
     }
 }
