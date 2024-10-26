f89efdde0f271b947d0df05b0e34e0afc3871d3c
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-26 21:53:49 +0100

    Combining multiple patterns with a match guard

--- ch18-03-pattern-syntax/example/src/main.rs
+++ ch18-03-pattern-syntax/example/src/main.rs
@@ -183,6 +183,14 @@ fn main() {
     }
 
     println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
 }
 
 fn foo(_: i32, y: i32) {
