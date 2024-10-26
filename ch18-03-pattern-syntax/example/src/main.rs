da15769953f9e595c05d6b8fd54820cc393853c1
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-10-26 21:53:15 +0100

    Using a match guard to test for equality with an outer variable

--- ch18-03-pattern-syntax/example/src/main.rs
+++ ch18-03-pattern-syntax/example/src/main.rs
@@ -172,6 +172,17 @@ fn main() {
             println!("Some numbers: {first}, {last}");
         }
     }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
 }
 
 fn foo(_: i32, y: i32) {
